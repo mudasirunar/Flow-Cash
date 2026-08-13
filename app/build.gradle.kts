5f51f4b5ba1ab049d8d968fa237f26a49905d3c9
Mudasir Ali <unarmudasir@gmail.com>
2026-08-13 19:44:29 +0500

    feat: increment version code to 4 and update version name to 2.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.mudasir.flowcash"
         minSdk = 24
         targetSdk = 35
-        versionCode = 3
-        versionName = "2.1"
+        versionCode = 4
+        versionName = "2.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
